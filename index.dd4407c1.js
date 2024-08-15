3d68376426aa7c7729f1e790dc64c1afe75f64a8
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-08-15 23:12:39 +0300

    make build

---
+++ index.dd4407c1.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=t.clientHeight,c=t.clientWidth,i=e.getBoundingClientRect(),o=(c-i.height)/2,n=(l-i.width)/2;e.style.position="absolut",e.style.top="".concat(o,"px"),e.style.left="".concat(n,"px");
+//# sourceMappingURL=index.dd4407c1.js.map
